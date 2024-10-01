d27e490ade2d7a03cdca3531ef97be0fe2b149a4
Sneha Soni <baberwalsnehaa@gmail.com>
2024-10-01 12:29:34 +0530

    Changes Done

--- src/custom.js
+++ src/custom.js
@@ -1,9 +1,2 @@
 
-    let AllQuestions = document.querySelectorAll(".FaqQue");
-    AllQuestions.forEach((e,i)=>{
-        e.addEventListener("Click",()=>{
-            e.nextElementSibling.classList.add("h-[auto]")
-            e.nextElementSibling.classList.add("scale-y-100")
-            e.nextElementSibling.classList.add("p-[20px]")
-        })
-})
+
